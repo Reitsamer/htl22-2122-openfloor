f9c8f5d9a74e2cbf7aad6c59a0d3a9c412332281
Wolf Reitsamer <reit@htl-donaustadt.at>
2022-02-17 16:30:41 +0100

    Karte drehen, OnClick Callback, Singleton in CardManager

--- Memory/Assets/Scripts/CardButton.cs
+++ Memory/Assets/Scripts/CardButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,50 @@ using UnityEngine.UI;
 
 public class CardButton : MonoBehaviour
 {
+    public Material backMaterial;
+    private Material frontMaterial;
+
+    private bool showFrontMaterial = false;
+
+    private int index;
+    
+    private void Start()
+    {
+        SetUsedMaterial();
+    }
+
+    public void SetIndex(int index)
+    {
+        this.index = index;
+    }
+
     public void SetMaterial(Material material)
     {
-        this.GetComponent<Image>().material = material;
+        // this.GetComponent<Image>().material = material;
+        frontMaterial = material;
+    }
+
+    public void OnClicked()
+    {
+        CardManager.Instance.CardClicked(index);
+    }
+    
+    public void ChangeMaterial()
+    {
+        showFrontMaterial = !showFrontMaterial;
+        
+        SetUsedMaterial();
+    }
+
+    private void SetUsedMaterial()
+    {
+        if (showFrontMaterial)
+        {
+            this.GetComponent<Image>().material = frontMaterial;
+        }
+        else
+        {
+            this.GetComponent<Image>().material = backMaterial;
+        }
     }
 }
--- Memory/Assets/Scripts/CardManager.cs
+++ Memory/Assets/Scripts/CardManager.cs
@@ -9,6 +9,29 @@ public class CardManager : MonoBehaviour
     public CardButton[] cards;
     public Material[] materials;
 
+    // private List<int> selectedCards = new List<int>();
+    private int[] selectedCards = new int[2];
+    private int selectedCardsCount = 0;
+    
+    private static CardManager instance;
+    public static CardManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private CardManager()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void Start()
     {
         List<Material> listOfMaterials = new List<Material>(materials.Length * 2);
@@ -23,8 +46,25 @@ public class CardManager : MonoBehaviour
         for (int i = 0; i < cards.Length; i++)
         {
             int index = Random.Range(0, listOfMaterials.Count);
+            cards[i].SetIndex(i);
             cards[i].SetMaterial(listOfMaterials[index]);
             listOfMaterials.RemoveAt(index);
         }
+    }
+
+    public void CardClicked(int index)
+    {
+        Debug.Log($"Card clicked: #{index}");
+        
+        // selectedCardsCount++;
+        // if (selectedCardsCount == 1)
+        // {
+        //     
+        // }
+        //
+        // if selectedCardsCount == 2)
+        // {
+        //     
+        // }
     }
 }
